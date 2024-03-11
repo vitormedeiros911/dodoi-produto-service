e7f9360925f8fbc78243fece488a8db84c5577e4
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-03-11 01:21:50 -0300

    chore: Altera aplicação para funcionar como microserviço e adiciona comunicação com broker

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main');
+const configService = new ConfigService();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          `amqp://${configService.get<string>('RMQ_USER')}:${configService.get<string>('RMQ_PASSWORD')}@${configService.get<string>('RMQ_HOST')}`,
+        ],
+        queue: 'produtos',
+      },
+    },
+  );
+
+  await app.listen().then(() => logger.log('Executando microserviço'));
 }
 bootstrap();
