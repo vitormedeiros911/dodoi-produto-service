22a56c796e83de5f9212353f20a214b481b8df53
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-05-27 08:40:59 -0300

    fix: Correção na criação de um produto

--- src/produto/produto.service.ts
+++ src/produto/produto.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { v4 as uuid } from 'uuid';
 
 import { Produto } from './schema/produto.schema';
 
@@ -10,7 +11,14 @@ export class ProdutoService {
     @InjectModel('Produto') private readonly produtoModel: Model<Produto>,
   ) {}
 
-  async criarProduto(produto: Produto): Promise<Produto> {
-    return new this.produtoModel(produto).save();
+  async criarProduto(produto: Produto): Promise<void> {
+    const id = uuid();
+
+    const novoProduto = new this.produtoModel({
+      id,
+      ...produto,
+    });
+
+    novoProduto.save();
   }
 }
